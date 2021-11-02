306363ea83c73905d2b2f85a88369b3cc41deeb2
SoufCoding <75219279+SoufCoding@users.noreply.github.com>
2021-11-03 00:32:49 +0100

    Search Input
    Add search Input

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('heelo');
+let searchIcon = document.querySelector('#search');
+let searchInput = document.querySelector('.input-container');
+let searchClose = document.querySelector('#close');
+
+searchIcon.onclick =function () {
+    if (searchInput.style.left = '100%') {
+        searchInput.style.left = '0';
+        searchIcon.style.display = 'none';
+        searchClose.style.display = 'block';
+    }
+};
+
+searchClose.onclick =function () {
+    if (searchInput.style.left = '0') {
+        searchInput.style.left = '100%';
+        searchIcon.style.display = 'block';
+        searchClose.style.display = 'none';
+    }
+}
